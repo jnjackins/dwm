4b1c9e1b585174f79fe1b61da92f658da550ec2b
Jeremy Jackins <jeremyjackins@gmail.com>
2020-07-07 10:24:38 -0700

    config updates

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 4;        /* gap between tiles */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 8;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "tamsyn:size=12" };
@@ -56,24 +56,26 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static char dmenumon[2] = "0"; /* manipulated in spawn() */
 static const char *termcmd[]  = { "9term", NULL };
+static const char *runcmd[] = { "dmenu_run", "-m", dmenumon, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *plumbcmd[] = { "dmenu_plumb", "-m", dmenumon, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_Return, spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Tab,    spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_F1,     spawn,          {.v = runcmd } },
+	{ MODKEY,                       XK_F2,     spawn,          {.v = plumbcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Up,     focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_Down,   focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_Left,   setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_Right,  setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },   // dedicated key (right upper)
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },   // dedicated key (right lower)
-	{ MODKEY,                       XK_z,      zoom,           {0} },          // dedicated key (left upper)
-	{ MODKEY,                       XK_v,      view,           {0} },          // dedicated key (left lower)
-	{ MODKEY,                       XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_Prior,  incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_Next,   incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_End,    zoom,           {0} },
+	{ MODKEY,                       XK_Home,   view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -113,4 +115,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
